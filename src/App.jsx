a5e6e9b29c51610cc5c1e6ea330d5a197f513a28
unknown <roniriman386@gmail.com>
2023-09-11 22:13:53 +0600

    Start Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div >
+      <Navbar/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+              <h1 className='text-center font-bold  text-4xl'>Knowledge Cafe </h1>
+        </div>
+    );
+};
+
+export default Navbar;
